80a28617e1eecc8a8fdd6231f4143b37b9e59ea6
ac3ss0r <grobnikolaj@gmail.com>
2023-06-05 08:57:05 +0300

    2.1 update
    Fixed rebooting (now frida is launched from /data/local/tmp and not from /data/data/acessor.droidfrida). Updated icon sizes. Reworked CodeView (implemented word wrapping support). Added additional settings (attach to existing process).

---
+++ app/src/main/java/acessor/droidfrida/ui/FixedSpinner.java
@@ -0,0 +1,31 @@
+package acessor.droidfrida.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Spinner;
+
+/* The default spinner doesnt fire its events 
+ when the selection is made, thats a bug in 
+ standard android library. To fix that this
+ class was made
+ */
+
+public class FixedSpinner extends Spinner {
+	OnItemSelectedListener listener;
+
+	public FixedSpinner(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	public void setSelection(int position) {
+		super.setSelection(position);
+		if (listener != null)
+			listener.onItemSelected(null, null, position, 0);
+	}
+
+	public void setOnItemSelectedEvenIfUnchangedListener(
+		OnItemSelectedListener listener) {
+		this.listener = listener;
+	}
+}
